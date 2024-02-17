6e656613d3aaa4eef60b56a6309c3c5bcd0ec2d5
seajee <teapods@proton.me>
2024-02-17 13:09:46 +0100

    Reorganized pages their fetching

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const page1 = document.getElementsByTagName("button")[0];
+const buttons = document.getElementsByTagName("button");
 const main = document.getElementsByClassName("main")[0];
 
-page1.onclick = function() {
-    fetch("test.html")
-        .then(response => response.text())
-        .then(data => {
-            main.innerHTML = data;
-        })
-        .catch(error => {
-            console.error(error);
-            main.innerHTML = "<p>Error: Could not fetch test.txt</p>";
-        });
-};
+for (let i = 0; i < buttons.length; ++i) {
+    const button = buttons[i];
+
+    button.onclick = function() {
+        fetch(button.innerHTML)
+            .then(response => response.text())
+            .then(data => {
+                main.innerHTML = data;
+            })
+            .catch(error => {
+                console.error(error);
+                main.innerHTML = "<p>Error: Could not fetch test.txt</p>";
+            });
+    }
+}
